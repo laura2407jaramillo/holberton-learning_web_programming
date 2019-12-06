0f61220c5428a1088f58637dd84904703768fe13
laura2407jaramillo <lasoja07@gmail.com>
2019-12-06 15:41:56 -0500

    Varible

---
+++ my_first_javascript_variable.js
@@ -0,0 +1,10 @@
+let my_variable = I love Javascript!
+undefined
+console.log(my_variable)
+I love Javascript
+undefined
+let variable_text ="Holberton"
+undefined
+console.log(variable_text)
+Holberton
+undefined
